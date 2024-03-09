2499a7ceca1541d624cba7117b212dfb7fad6c91
Maksim459 <159301480+Maksim459@users.noreply.github.com>
2024-03-09 09:12:13 +0300

    Add files via upload

---
+++ 8.1/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Item item = new Item();
+
+        Console.WriteLine("введите значение");
+        string a = Console.ReadLine();
+        item.SetName(a);
+
+        
+        Console.WriteLine($"Имя предмета: {item.GetName()}");
+    }
+   
+}
+public class Item()
+{
+    private string Name = "unkown";
+
+
+    public void SetName(string newName)
+    {
+
+        Name= newName;
+    }
+    public string GetName()
+    {
+        return Name;
+    }
+}
---
+++ 8.2/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+class Engine
+{
+    public void Start()
+    {
+        Console.WriteLine("двигатель запущен");
+    }
+}
+abstract class Transport
+{
+    public Engine Engine { get; set; }
+    public abstract void Move();
+}
+class Car : Transport
+{
+    public Car()
+    { 
+     Engine = new Engine();
+    }
+    public override void Move()
+    {
+        Engine.Start();
+        Console.WriteLine("машина едет");
+    }
+}
+class Plane : Transport
+{
+    public Plane()
+    {
+        Engine = new Engine();
+    }
+    public override void Move()
+    {
+       
+        Engine.Start();
+        Console.WriteLine("самолет летит");
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var car = new Car();
+        car.Move();
+
+        var plane = new Plane();
+        plane.Move();
+    }
+}
