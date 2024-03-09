35e0311cd64a31ec0a00219475f8c82322688958
Maksim459 <159301480+Maksim459@users.noreply.github.com>
2024-03-09 09:14:04 +0300

    Add files via upload

---
+++ 8.3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Program program = new Program();
+        Console.WriteLine(program.Method());
+
+    }
+    public int Method()
+    {
+        int[] array = { 100, 2, 39, 4, 5, 3, 456, 65, 765, 56, 76 };
+        int i = array.Length;
+        int a = 0;
+        int b = 1;
+        int item = 0;
+        int a1 = array[a];
+
+        while (i > 1)
+        {
+            int b1 = array[b];
+            if (a1 > b1)
+            {
+                item = a1;
+            }
+            else
+            {
+                item = b1;
+            }
+            a1 = item;
+
+            b++;
+
+            i--;
+
+
+
+        }
+       
+        return item;
+    }
+}
---
+++ 8.4/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Bag bag = new Bag();
+        bag.Open();
+        bag.Items();
+        bag.ItemsInBag();
+
+    }
+}
+
+class Bag
+{
+    private string[] Item = new string[8];
+    bool isOpen = false;
+    public bool Open()
+    {
+        Console.WriteLine("открыть сумку y/n?");
+        string b = Console.ReadLine();
+        if (b == "y")
+        {
+            Console.WriteLine("Сумка открыта");
+            isOpen = true;
+
+        }
+        
+        return isOpen;
+    }
+    public void Items()
+    {
+        int n = 0;
+        if (isOpen == true)
+        {
+            int i = 8;
+            do
+            {
+                Console.WriteLine("введите то что хотите положить в сумку");
+                string itembag=Console.ReadLine();
+                
+                
+                Item[n] = itembag;
+                n++;
+                i--;
+                Console.WriteLine("предмет {0} помещен в ячейку {1}",itembag,n);
+            }
+            while (i > 0);
+            Console.WriteLine("сумка заполнена");
+        }
+       
+    }
+    public void ItemsInBag()
+    {
+        string yn;
+        if (isOpen == true)
+        {
+            do
+            {
+                Console.WriteLine("введите номер предмета,который хотите достать");
+                int y = Convert.ToInt32(Console.ReadLine());
+                if (y <= 7)
+                {
+                    Console.WriteLine($"вы достали {Item[y]}");
+                }
+                else
+                {
+                    Console.WriteLine("такой ячейки нет");
+                }
+                Console.WriteLine("хотите продолжить y/n?");
+                yn=Console.ReadLine();
+            } while (yn=="y");
+        }
+        Console.WriteLine("Сумка закрыта");
+        Console.ReadLine();
+        
+    
+    }    
+    
+    
+}  
